413dc6cc549689a6e428a38b986c8842cd36b064
Sh1raz <shrzdev@gmail.com>
2015-08-05 21:56:08 -0300

    Atualizado conforme Hercules.

--- src/map/script.c
+++ src/map/script.c
@@ -5769,6 +5769,7 @@ BUILDIN(warp)
 {
 	int ret;
 	int x,y;
+	int warp_clean = 1;
 	const char* str;
 	TBL_PC* sd;
 
@@ -5779,6 +5780,12 @@ BUILDIN(warp)
 	str = script_getstr(st,2);
 	x = script_getnum(st,3);
 	y = script_getnum(st,4);
+	
+	if (script_hasdata(st, 5)) {
+		warp_clean = script_getnum(st, 5);
+	}
+
+	sd->state.warp_clean = warp_clean;
 
 	if(strcmp(str,"Random")==0)
 		ret = pc->randomwarp(sd,CLR_TELEPORT);
@@ -19888,7 +19895,7 @@ void script_parse_builtin(void) {
 		BUILDIN_DEF(jobchange,"i?"),
 		BUILDIN_DEF(jobname,"i"),
 		BUILDIN_DEF(input,"r??"),
-		BUILDIN_DEF(warp,"sii"),
+		BUILDIN_DEF(warp,"sii?"),
 		BUILDIN_DEF(areawarp,"siiiisii??"),
 		BUILDIN_DEF(warpchar,"siii"), // [LuzZza]
 		BUILDIN_DEF(warpparty,"siii?"), // [Fredzilla] [Paradox924X]
