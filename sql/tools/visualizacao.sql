cc28b85324c457eb511799b0db0ff2e6dd6cb67c
Protimus <rafaelgorges@outlook.com>
2015-12-08 02:52:52 -0200

    Criado arquivo para futuras visualizações (views) em SQL

---
+++ sql/tools/visualizacao.sql
@@ -0,0 +1,19 @@
+/*_________________________________________________________________________
+ /                                                                         \
+ |                  _           _   _   _                                  |
+ |                 | |__  _ __ / \ | |_| |__   ___ _ __   __ _             |
+ |                 | '_ \| '__/ _ \| __| '_ \ / _ \ '_ \ / _` |            |
+ |                 | |_) | | / ___ \ |_| | | |  __/ | | | (_| |            |
+ |                 |_.__/|_|/_/   \_\__|_| |_|\___|_| |_|\__,_|            |
+ |                                                                         |
+ |                       brAthena © 2015 - Banco de Dados                  |
+ |                                Visualizações                            |
+ | - Arquivo contendo consultas por meio de SELECT nas quais geram-se,     |
+ | visualizações para maior facilidade na consulta de tarefas simples,     |
+ | podendo liberar acesso restrito a um moderador ou grupo de usuários...  |
+ | até mesmo a jogadores, para maiores informações                         |
+ \_________________________________________________________________________/
+*/
+
+-- Visualização: Explicação
+--SELECT CAMPOS FROM TABELA WHERE CONDIÇÕES
