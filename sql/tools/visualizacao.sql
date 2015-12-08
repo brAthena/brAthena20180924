af679b178166379f5a32e5500b0fa752e556df68
Protimus <rafaelgorges@outlook.com>
2015-12-08 11:55:26 -0200

    Criadas primeiras estruturas de visualização para logs

--- sql/tools/visualizacao.sql
+++ sql/tools/visualizacao.sql
@@ -15,5 +15,14 @@
  \_________________________________________________________________________/
 */
 
--- Visualização: Explicação
---SELECT CAMPOS FROM TABELA WHERE CONDIÇÕES
+-- IMPORTANTE: Valores fictíssios para IDs, altere conforme sua necessidade.
+
+-- Verifica todos os comandos de '@' de uma conta especifica.
+-- Útil para disponibilizar o que GMs fazem no servidor ou para pesquisa mais rápida nos logs por parte do admin
+-- Para adicionar mais de uma conta, utilize a cláusula "AND".
+CREATE VIEW comando_gm AS SELECT char_name, command FROM atcommandlog WHERE account_id = 1; 
+
+-- Verifica cartas especificass que foram dropadas no servidor.
+-- Útil para disponibilizar verificações de cartas de MvP e quem as têm, para mostrar
+-- transparência no servidor.
+CREATE VIEW carta_dropada AS SELECT CharName, CardName FROM card_log WHERE Card_ID = 1; 
